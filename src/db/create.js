c949429d7b72818a1a9cba71ee5af886aa37c3e7
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-16 18:32:08 -0300

    better database creation script

--- src/db/create.js
+++ src/db/create.js
@@ -1,19 +1,42 @@
 const qb = require('./')
+const { DB_DATABASE } = process.env
+
+const connection = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  },
+  debug: false
+})
 
 async function run () {
-  await qb.raw('select 1;')
+  try {
+    await qb.raw('select 1;')
+  } catch (err) {
+    console.log(`Database "${DB_DATABASE}" is not created. Creating...`)
+  }
 
-  await qb.schema.createTableIfNotExists('users', (table) => {
-    table.increments('id')
-    table.string('name')
-    table.string('email')
-    table.string('password')
-    table.string('role').defaultTo('USER')
-    table.timestamp('created_at').defaultTo(qb.raw('now()'))
-    table.timestamp('updated_at').defaultTo(qb.raw('now()'))
-  })
+  await connection.raw(`CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};`)
+
+  await qb.raw(`
+  CREATE TABLE \`users\` (
+    \`id\` bigint(20) NOT NULL AUTO_INCREMENT,
+    \`email\` varchar(255) NOT NULL,
+    \`name\` varchar(255) NOT NULL,
+    \`password\` varchar(255) NOT NULL,
+    \`facebook_token\` varchar(255) DEFAULT NULL,
+    \`facebook_id\` varchar(255) DEFAULT NULL,
+    \`role\` varchar(255) DEFAULT 'USER',
+    \`created_at\` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    \`updated_at\` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (\`id\`),
+    UNIQUE KEY \`usuario_email_unique\` (\`email\`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`)
 
   console.log('Done!')
+
   process.exit()
 }
 
