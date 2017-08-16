b234e47db18bb44a05bfe0cea7cd77fae707d85e
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-16 19:24:29 -0300

    add jobs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const boolParser = require('express-query-boolean')
 const app = require('./src/app/')
+const jobs = require('./src/jobs')
 const errorHandlers = require('./src/util/error-handlers')
 const expressValidator = require('express-validator')
 
@@ -18,6 +19,7 @@ server.use(express.static('public'))
 server.use(morgan('dev', { skip: () => process.env.NODE_ENV === 'test' }))
 
 app.init(server)
+jobs.init()
 
 server.use(errorHandlers.notFound)
 server.use(errorHandlers.validationError)
---
+++ src/jobs/index.js
@@ -0,0 +1,13 @@
+const schedule = require('node-schedule')
+
+function init() {
+  remindToDrinkWater()
+}
+
+function remindToDrinkWater() {
+  schedule.scheduleJob('0 */2 * * *', () => {
+    console.log('\n Reminder: go get a cup of water 💧\n')
+  })
+}
+
+module.exports = { init }
