ecb55a47f1b134720efa78aea8e3ad022b5d2a2f
Ruan Martinelli <martinelliruan@gmail.com>
2017-08-16 19:25:48 -0300

    linter

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,10 +1,10 @@
 const schedule = require('node-schedule')
 
-function init() {
+function init () {
   remindToDrinkWater()
 }
 
-function remindToDrinkWater() {
+function remindToDrinkWater () {
   schedule.scheduleJob('0 */2 * * *', () => {
     console.log('\n Reminder: go get a cup of water 💧\n')
   })
