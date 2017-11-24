fe908c78776ebfa5c47d5c9adb16cbe9d63dff5e
Ruan Martinelli <martinelliruan@gmail.com>
2017-11-24 12:35:10 -0200

    use joi to validate env vars

--- src/util/check-env-vars.js
+++ src/util/check-env-vars.js
@@ -1,24 +1,29 @@
-const environmentVariables = [
-  'APP_NAME',
-  'APP_PORT',
-  'DB_HOST',
-  'DB_USER',
-  'DB_DATABASE',
-  'JWT_SECRET',
-  'JWT_EXPIRES_IN'
-]
+import joi from 'joi'
+import validateSchema from 'util/validate-schema'
+
+// prettier-ignore
+const envVarsSchema = joi
+  .object({
+    APP_NAME: joi.string().required(),
+    APP_PORT: joi.number().required(),
+    DB_HOST: joi.string().hostname().required(),
+    DB_USER: joi.string().required(),
+    DB_DATABASE: joi.string().required(),
+    JWT_SECRET: joi.string().token().min(10).required(),
+    JWT_EXPIRES_IN: joi.string().required()
+  })
+  .unknown()
+  .required()
 
 /**
  * Checks if all the required environment variables are present.
  *
  * @throws {Error} Will throw an error if one variable is missing
  */
-function checkEnvVars () {
-  for (const variable of environmentVariables) {
-    if (!process.env[variable]) {
-      throw new Error(`Environment variable ${variable} is missing`)
-    }
-  }
+function checkEnvVars() {
+  const { error: err, value } = joi.validate(process.env, envVarsSchema)
+
+  if (err) throw new Error(`Env error: ` + err.details[0].message)
 }
 
 export default checkEnvVars
